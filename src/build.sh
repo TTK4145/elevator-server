f9541b8ceebe748a6a51c108617963a88aee96a7
Anders Petersen <klasbo@gmail.com>
2022-01-21 00:01:21 +0100

    Update to Arduino elevator

--- src/build.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ../target
-gcc -std=gnu11 -Wall -c elev.c -o ../target/elev.o;
-gcc -std=gnu11 -Wall -c io.c -o ../target/io.o;
-dmd -w -g -of../target/ElevatorServer elevatorserver.d ../target/elev.o ../target/io.o -L-lcomedi -L-lm
